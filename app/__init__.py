adde97c9cd9083db528f638a2ee143835f15348d
Gad Wissberg <gadw17@gmail.com>
2020-02-15 23:23:24 +0200

    Added DB.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from sqlalchemy import create_engine
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+engine = create_engine('postgresql://postgres:fatcow@localhost/mututale')
+connection = engine.connect()
+print("DB Instance created")
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from flask import render_template
 
 from app import app
+from app import connection
 
 
 @app.route('/get_stories')
 def index():
     return render_template('stories_list.json')
+
+
+@app.route('/test')
+def test():
+    fetch_query = connection.execute("SELECT * FROM users")
+    for data in fetch_query.fetchall():
+        return str(data)
