5240c6a8e365b21f983b9dfd1f09c045c0930aa4
Gad Wissberg <gadw17@gmail.com>
2020-03-12 12:10:41 +0200

    Cosmetics.

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,6 @@ from .auth import auth as auth_blueprint
 
 app.register_blueprint(auth_blueprint)
 
-from .main import main as main_blueprint
+from app.routes.main import main as main_blueprint
 
 app.register_blueprint(main_blueprint)
